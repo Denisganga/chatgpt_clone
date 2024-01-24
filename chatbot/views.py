ea62a72940712da6d7f35092ecb6f934e016e527
Denis Nganga <denisnganga16@gmail.com>
2024-01-24 21:00:59 +0300

    tackling the register view

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import JsonResponse
 import openai
+
+from django.contrib import auth 
+from django.contrib.auth.models import User
 
 
 openai_api_key='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'
@@ -29,3 +32,32 @@ def chatbot(request):
         response = ask_openai(message)
         return JsonResponse({'message':message, 'response':response})
     return render(request, 'chatbot.html')
+
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+
+        if password1==password2:
+            try:
+                user=User.objects.create_user(username,email,password1)
+                user.save()
+                auth.login(request,user)
+                return redirect('chatbot')
+            except:
+                error_message='Error creating account'
+                return render(request, 'register.html', {'error_message':error_message})
+        else:
+            error_message = 'password dont match'
+            return render(request,'register.html', {'error_message':error_message})
+
+    return render(request,'register.html')
+
+def logout(request):
+    auth.logout(request)
