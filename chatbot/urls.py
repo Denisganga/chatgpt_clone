091145fc70f863cee351e1dadb6e3a9b07126c08
Denis Nganga <denisnganga16@gmail.com>
2024-01-24 20:35:58 +0300

    defined the url pattern for the login,register and logout

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.chatbot, name='chatbot')
+    path('',views.chatbot, name='chatbot'),
+    path('login',views.login, name='login'),
+    path('register',views.register, name='register'),
+    path('logout',views.logout, name='logout')
+
+
+    
 ]
+
+
+
+
+
