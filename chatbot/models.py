18299e08c8aad6ae69ce2ce11c63cb8977929d4c
Denis Nganga <denisnganga16@gmail.com>
2024-01-26 19:06:55 +0300

    defined the models to keep history of the user

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Chat(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    message=models.TextField()
+    response=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
