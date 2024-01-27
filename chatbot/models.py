2dcb243c91daa813c2ec28ee924fdf7b671c0bba
Denis Nganga <denisnganga16@gmail.com>
2024-01-28 01:15:42 +0300

    made migrations and created the superuser

--- chatbot/models.py
+++ chatbot/models.py
@@ -9,3 +9,6 @@ class Chat(models.Model):
     message=models.TextField()
     response=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}:{self.message}'
