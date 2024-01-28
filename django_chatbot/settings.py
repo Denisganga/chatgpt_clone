2d2e98bc507f531d2b2a0492e30678a3f9a38bd4
Denis Nganga <denisnganga16@gmail.com>
2024-01-28 19:01:43 +0300

    testing and reviewing the chatbot

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+import os
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
